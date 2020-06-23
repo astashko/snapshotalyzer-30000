df096d62ba403e961b6c025a31a862c02fe9fad1
Andriy Stashko <andriy.stashkoziabletsov@gmail.com>
2020-06-23 22:20:23 +0300

    Safe start/stop

--- README.md
+++ README.md
@@ -16,6 +16,6 @@ shotty uses the configuration file created by the AWS cli. e.g.
 
 `pipenv run python shotty/shotty.py <command> <subcommand> <--project=PROJECT>`
 
-*command* is instances, snapshots, or volumes
-*subcommand* depends on command
+*command* is instances, snapshots, or volumes  
+*subcommand* depends on command  
 *project* is optional
--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,4 +1,5 @@
 import boto3
+import botocore
 import click
 
 session = boto3.Session(profile_name='shotty')
@@ -21,7 +22,7 @@ def instances():
 
 @instances.command('list')
 @click.option('--project', default = None,
-    help = "Only instances for project (tag Project:<name>")
+    help = "Only instances for project (tag Project:<name>)")
 def list_instances(project):
     "List EC2 instances"
     
@@ -40,40 +41,54 @@ def list_instances(project):
 
 @instances.command('stop')
 @click.option('--project', default = None,
-    help = "Only instances for project (tag Project:<name>")
+    help = "Only instances for project (tag Project:<name>)")
 def stop_instances(project):
     "Stop EC2 instances"
 
     for i in get_instances(project):
         print("Stopping {0}...".format(i.id))
-        i.stop()
+        try:
+            i.stop()
+        except botocore.exceptions.ClientError as e:
+            print(" Could not stop {0}. ".format(i.id) + str(e))
 
     return
 
 @instances.command('start')
 @click.option('--project', default = None,
-    help = "Only instances for project (tag Project:<name>")
+    help = "Only instances for project (tag Project:<name>)")
 def start_instances(project):
     "Start EC2 instances"
 
     for i in get_instances(project):
         print("Starting {0}...".format(i.id))
-        i.start()
+        try:
+            i.start()
+        except botocore.exceptions.ClientError as e:
+            print(" Could not start {0}. ".format(i.id) + str(e))
 
     return
 
 @instances.command('snapshot', help = 'Create snapshots of all volumes')
 @click.option('--project', default = None,
-    help = "Only instances for project (tag Project:<name>")
+    help = "Only instances for project (tag Project:<name>)")
 def create_snapshots(project):
     "Create snapshots for EC2 instances"
 
     for i in get_instances(project):
+        print("Stopping {0}...".format(i.id))
         i.stop()
+        i.wait_until_stopped()
+        
         for v in i.volumes.all():
-            print("Creating snapshot of {0}".format(v.id))
+            print("  Creating snapshot of {0}".format(v.id))
             v.crate_snapshot(Description = "Created by Snapshotalyzer 30000")
+        
+        print("Starting {0}...".format(i.id))
+        i.start()
+        i.wait_until_running()
 
+    print('Jon''s done!')
     return
 
 @cli.group()
@@ -82,7 +97,7 @@ def volumes():
 
 @volumes.command('list')
 @click.option('--project', default = None,
-    help = "Only volumes from instances for project (tag Project:<name>")
+    help = "Only volumes from instances for project (tag Project:<name>)")
 def list_volumes(project):
     "List EC2 volumes"
 
@@ -104,10 +119,12 @@ def list_volumes(project):
 def snapshots():
     """Commands for snapshots"""
 
-@volumes.command('list')
+@snapshots.command('list')
 @click.option('--project', default = None,
-    help = "Only snapshots from instances for project (tag Project:<name>")
-def list_snapshots(project):
+    help = "Only snapshots from instances for project (tag Project:<name>)")
+@click.option('--all', 'list_all', default = False, is_flag = True,
+    help = "List all snapshots for each volume, not just thr most recent")
+def list_snapshots(project, list_all):
     "List EC2 snapshots"
 
     instances = get_instances(project)
@@ -123,6 +140,8 @@ def list_snapshots(project):
                     s.progress,
                     s.start_time.strftime("%c")
                 )))
+
+                if not list_all and s.state == "completed": break
     
     return
 
