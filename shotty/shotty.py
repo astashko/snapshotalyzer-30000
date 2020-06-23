6ef089f8eb3786a00e89549b88dc39b0ddac735f
Andriy Stashko <andriy.stashkoziabletsov@gmail.com>
2020-06-23 22:33:40 +0300

    Skip if snapshot in progress

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -12,6 +12,10 @@ def get_instances(project):
     else:
         return ec2.instances.all()
 
+def has_pending_snapshot(volume):
+    snapshots = list(volume.snapshots.all())
+    return snapshots and snapshots[0].state == 'pending'
+
 @click.group()
 def cli():
     """Shotty manages snapshots"""
@@ -81,6 +85,10 @@ def create_snapshots(project):
         i.wait_until_stopped()
         
         for v in i.volumes.all():
+            if (has_pending_snapshot(v)):
+                print("  Skipping {0}, snapshot already in progress".format(v.id))
+                continue
+
             print("  Creating snapshot of {0}".format(v.id))
             v.crate_snapshot(Description = "Created by Snapshotalyzer 30000")
         
