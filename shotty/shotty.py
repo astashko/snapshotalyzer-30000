0f0c351d61099e55c6568dbaed843d99be8e70f0
Andriy Stashko <andriy.stashkoziabletsov@gmail.com>
2020-06-23 21:41:21 +0300

    Added volumes and snapshots command

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -12,6 +12,10 @@ def get_instances(project):
         return ec2.instances.all()
 
 @click.group()
+def cli():
+    """Shotty manages snapshots"""
+
+@cli.group()
 def instances():
     """Commands for instances"""
 
@@ -58,5 +62,69 @@ def start_instances(project):
 
     return
 
+@instances.command('snapshot', help = 'Create snapshots of all volumes')
+@click.option('--project', default = None,
+    help = "Only instances for project (tag Project:<name>")
+def create_snapshots(project):
+    "Create snapshots for EC2 instances"
+
+    for i in get_instances(project):
+        i.stop()
+        for v in i.volumes.all():
+            print("Creating snapshot of {0}".format(v.id))
+            v.crate_snapshot(Description = "Created by Snapshotalyzer 30000")
+
+    return
+
+@cli.group()
+def volumes():
+    """Commands for volumes"""
+
+@volumes.command('list')
+@click.option('--project', default = None,
+    help = "Only volumes from instances for project (tag Project:<name>")
+def list_volumes(project):
+    "List EC2 volumes"
+
+    instances = get_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            print(", ".join((
+                v.id,
+                i.id,
+                v.state,
+                str(v.size) + "GiB",
+                v.encrypted and "Encrypted" or "Not Encrypted"
+            )))
+    
+    return
+
+@cli.group()
+def snapshots():
+    """Commands for snapshots"""
+
+@volumes.command('list')
+@click.option('--project', default = None,
+    help = "Only snapshots from instances for project (tag Project:<name>")
+def list_snapshots(project):
+    "List EC2 snapshots"
+
+    instances = get_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            for s in v.snapshots.all():
+                print(", ".join((
+                    s.id,
+                    v.id,
+                    i.id,
+                    v.state,
+                    s.progress,
+                    s.start_time.strftime("%c")
+                )))
+    
+    return
+
 if __name__ == '__main__':
-    instances()
+    cli()
